41de78529276fba06a7be1ee37be1cb535187965
Manel Montilla <manel.montilla@gmail.com>
2022-05-13 18:53:52 +0200

    Allow pointing directly to the directory of a checktype

--- pkg/checktypes/checktypes.go
+++ pkg/checktypes/checktypes.go
@@ -11,6 +11,7 @@ import (
 	"io/fs"
 	neturl "net/url"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -18,6 +19,8 @@ import (
 
 	"github.com/adevinta/vulcan-local/pkg/content"
 )
+
+var errNoChecktypeDir = errors.New("the directory does not contian a checktype")
 
 // ChecktypeRef represents a checktype with an optional prefix denoting the
 // repository (i.e. default/vulcan-zap vulcan-zap ).
@@ -58,7 +61,7 @@ type JSONChecktypes struct {
 func Import(repos []string, l log.Logger) (map[ChecktypeRef]Checktype, error) {
 	var checktypes = make(map[ChecktypeRef]Checktype)
 	for _, repo := range repos {
-		l.Debugf("Importing checktype from: %s", repo)
+		l.Debugf("Importing checktypes from: %s", repo)
 		repoURL, err := neturl.Parse(repo)
 		if err != nil {
 			return nil, err
@@ -102,6 +105,13 @@ func checktypesFromJSON(u *neturl.URL, l log.Logger) ([]Checktype, error) {
 	return checktypes, nil
 }
 
+// checktypesFromCode returns the checktypes info defined as code in a
+// directory. We sopport two forms of repositories:
+// One that contains only one check. It's indicated by a path that points
+// directly to directory containing the check, for instance "./vulcan-dkim".
+// The other that can point to more that one check. It's indicated by a path
+// pointing to the a directory that has one or more subdirectories containing
+// the code of the checks, for instance path: "./cmd/checks".
 func checktypesFromCode(u *neturl.URL, l log.Logger) ([]Checktype, error) {
 	path := strings.TrimPrefix(u.String(), "code://")
 	l.Debugf("Loading checktypes from code in: %s", path)
@@ -112,6 +122,19 @@ func checktypesFromCode(u *neturl.URL, l log.Logger) ([]Checktype, error) {
 	if !dirInfo.IsDir() {
 		return nil, fmt.Errorf("a code repository must point to a dir: %s", path)
 	}
+	var ct Checktype
+	// Check first is the dir points directly to a checktype.
+	if ct, err = readChecktype(path, l); err == nil {
+		return []Checktype{ct}, nil
+	}
+
+	if err != errNoChecktypeDir {
+		return nil, err
+	}
+	l.Debugf("Looking for checktypes defined in the subdirs of the dir %s", path)
+	// err==errNoChecktypeDir, so the directory itself does not contain a
+	// checktype. Check any if its subdirs contains one or more checktypes
+	// definitions.
 	entries, err := os.ReadDir(path)
 	if err != nil {
 		return nil, fmt.Errorf("error reading code checktypes repository %s: %v", path, err)
@@ -122,39 +145,49 @@ func checktypesFromCode(u *neturl.URL, l log.Logger) ([]Checktype, error) {
 			continue
 		}
 		dirpath := filepath.Join(path, e.Name())
-		l.Debugf("examing if directory %s contains the code of a check", dirpath)
-		manifestPath := filepath.Join(dirpath, "manifest.toml")
-		l.Debugf("trying to read manifest from %s", manifestPath)
-		m, err := ReadManifest(manifestPath)
-		// We consider a directory to be contain the code of a check if it has
-		// a manifest.
-		if errors.Is(err, fs.ErrNotExist) {
-			l.Debugf("The directory %s doesn't contains the code of a check", dirpath)
+		ct, err := readChecktype(dirpath, l)
+		if err == errNoChecktypeDir {
 			continue
 		}
 		if err != nil {
-			return nil, fmt.Errorf("error reading manifest file %s, %v", manifestPath, err)
-		}
-		options, err := m.UnmarshalOptions()
-		if err != nil {
-			return nil, fmt.Errorf("invalid options in manifest %s: %w", manifestPath, err)
-		}
-		assets, err := m.AssetTypes.Strings()
-		if err != nil {
-			return nil, fmt.Errorf("invalid asset types in manifest %s: %w", manifestPath, err)
-		}
-
-		ct := Checktype{
-			Name:         e.Name(),
-			Description:  m.Description,
-			Timeout:      m.Timeout,
-			Image:        fmt.Sprintf("code://%s", dirpath),
-			Options:      options,
-			RequiredVars: m.RequiredVars,
-			Assets:       assets,
+			return nil, err
 		}
 		checktypes = append(checktypes, ct)
 	}
 	return checktypes, nil
+}
 
+func readChecktype(dirpath string, l log.Logger) (Checktype, error) {
+	l.Debugf("Looking if the directory %s contains the code of a checktype", dirpath)
+	manifestPath := filepath.Join(dirpath, "manifest.toml")
+	l.Debugf("Trying to read manifest from %s", manifestPath)
+	m, err := ReadManifest(manifestPath)
+	// We consider a directory to be contain the code of a check if it has
+	// a manifest.
+	if errors.Is(err, fs.ErrNotExist) {
+		l.Debugf("The directory %s doesn't contain the code of a checktype", dirpath)
+		return Checktype{}, errNoChecktypeDir
+	}
+	if err != nil {
+		return Checktype{}, fmt.Errorf("error reading manifest file %s, %v", manifestPath, err)
+	}
+	options, err := m.UnmarshalOptions()
+	if err != nil {
+		return Checktype{}, fmt.Errorf("invalid options in manifest %s: %w", manifestPath, err)
+	}
+	assets, err := m.AssetTypes.Strings()
+	if err != nil {
+		return Checktype{}, fmt.Errorf("invalid asset types in manifest %s: %w", manifestPath, err)
+	}
+	name := path.Base(dirpath)
+	ct := Checktype{
+		Name:         name,
+		Description:  m.Description,
+		Timeout:      m.Timeout,
+		Image:        fmt.Sprintf("code://%s", dirpath),
+		Options:      options,
+		RequiredVars: m.RequiredVars,
+		Assets:       assets,
+	}
+	return ct, nil
 }
