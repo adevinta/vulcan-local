0cce771f0bd79f439ffef509b7581c9a8e8edce5
Jesus Carpintero <jesus.carpintero@adevinta.com>
2022-10-03 09:21:48 +0200

    Add test

--- pkg/generator/generator_test.go
+++ pkg/generator/generator_test.go
@@ -474,6 +474,19 @@ func TestGetTypesFromIdentifier(t *testing.T) {
 			},
 			wantErr: nil,
 		},
+		{
+			name: "Resolve to local GitRepository",
+			target: config.Target{
+				Target: ".",
+			},
+			want: []config.Target{
+				{
+					Target:    ".",
+					AssetType: "GitRepository",
+				},
+			},
+			wantErr: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
