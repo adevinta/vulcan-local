5aa867c4bcb7745802efd574d5b9330b8ea4c159
Faisal Soomro <faisal.soomro@schibsted.com>
2023-07-18 14:45:12 +0200

    adding support for GCPProject asset type

--- pkg/generator/generator_test.go
+++ pkg/generator/generator_test.go
@@ -484,6 +484,19 @@ func TestGetTypesFromIdentifier(t *testing.T) {
 			},
 			wantErr: nil,
 		},
+		{
+			name: "Resolve to GCPProject",
+			target: config.Target{
+				Target: "bagase234",
+			},
+			want: []config.Target{
+				{
+					Target:    "bagase234",
+					AssetType: "GCPProject",
+				},
+			},
+			wantErr: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
