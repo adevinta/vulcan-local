b59e1d1be855ee6fbfd2a4c5e40247b8676235ff
Roi Martin <jroi.martin@gmail.com>
2023-08-22 18:30:19 +0200

    pkg/dockerutil: detect Docker Desktop/Engine instead of Linux/macOS

--- pkg/dockerutil/dockerutil.go
+++ pkg/dockerutil/dockerutil.go
@@ -5,46 +5,87 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"runtime"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
 // GetGateways returns the gateways of the specified Docker network.
-func GetGateways(ctx context.Context, cli *client.Client, network string) ([]net.IP, error) {
+func GetGateways(ctx context.Context, cli *client.Client, network string) ([]*net.IPNet, error) {
 	resp, err := cli.NetworkInspect(ctx, network, types.NetworkInspectOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("network inspect: %w", err)
 	}
 
-	var gws []net.IP
+	var gws []*net.IPNet
 	for _, cfg := range resp.IPAM.Config {
-		gw := net.ParseIP(cfg.Gateway)
-		if gw == nil {
-			return nil, fmt.Errorf("invalid IP %q", cfg.Gateway)
+		_, subnet, err := net.ParseCIDR(cfg.Subnet)
+		if err != nil {
+			return nil, fmt.Errorf("invalid subnet: %v", cfg.Subnet)
 		}
-		gws = append(gws, gw)
-	}
 
+		ip := net.ParseIP(cfg.Gateway)
+		if ip == nil {
+			return nil, fmt.Errorf("invalid IP: %v", cfg.Gateway)
+		}
+
+		subnet.IP = ip
+		gws = append(gws, subnet)
+	}
 	return gws, nil
 }
 
-// GetBridgeHost returns a host that corresponds to the Docker host
-// that is reachable from the containers running in the default bridge
+// GetBridgeHost returns the gateway of the default Docker bridge
 // network.
+func GetBridgeGateway(cli *client.Client) (*net.IPNet, error) {
+	gws, err := GetGateways(context.Background(), cli, "bridge")
+	if err != nil {
+		return nil, fmt.Errorf("could not get Docker network gateway: %w", err)
+	}
+	if len(gws) != 1 {
+		return nil, fmt.Errorf("unexpected number of gateways: %v", len(gws))
+	}
+	return gws[0], nil
+}
+
+// GetBridgeHost returns a host that points to the Docker host and is
+// reachable from the containers running in the default bridge.
 func GetBridgeHost(cli *client.Client) (string, error) {
-	if runtime.GOOS != "linux" {
+	isDesktop, err := isDockerDesktop(cli)
+	if err != nil {
+		return "", fmt.Errorf("detect Docker Desktop: %w", err)
+	}
+
+	if isDesktop {
 		return "127.0.0.1", nil
 	}
 
-	gws, err := GetGateways(context.Background(), cli, "bridge")
+	gw, err := GetBridgeGateway(cli)
 	if err != nil {
-		return "", fmt.Errorf("could not get Docker network gateway: %w", err)
+		return "", fmt.Errorf("get bridge gateway: %w", err)
 	}
-	if len(gws) != 1 {
-		return "", fmt.Errorf("unexpected number of gateways: %v", len(gws))
+	return gw.IP.String(), nil
+}
+
+// isDockerDesktop returns true if the Docker daemon is part of Docker
+// Desktop. That means that there is a network interface with the same
+// IP of the gateway of the default Docker bridge network.
+func isDockerDesktop(cli *client.Client) (bool, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return false, fmt.Errorf("interface addrs: %w", err)
 	}
 
-	return gws[0].String(), nil
+	gw, err := GetBridgeGateway(cli)
+	if err != nil {
+		return false, fmt.Errorf("get bridge gateway: %w", err)
+	}
+
+	for _, addr := range addrs {
+		if gw.String() == addr.String() {
+			return false, nil
+		}
+	}
+	return true, nil
+
 }
